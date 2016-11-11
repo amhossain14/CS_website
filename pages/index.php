039c56b6fd7953c2c253168b16714d0f3bb43782
mattj122 <mattj122@gmail.com>
2016-11-10 21:10:16 -0600

    More fixes, added milestone_response doc

--- pages/index.php
+++ pages/index.php
@@ -3,25 +3,20 @@
 	<head>
 		<title>Computer Science</title>
 		<script src="../js/script.js" type="text/javascript"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="../css/style.css">
 		<link rel="icon" href="../back_logo/logow.png">
         <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Cuprum:400,400i,700,700i" rel="stylesheet">
         
 	</head>
 	<body>
-		<span id="wrapper">
+		<div id="wrapper">
 			<header>
-				<a href="faculty.html"><img  src="../back_logo/winonalogo.png" ></a>
-               <h1>Computer Science</h1> 
+				<div id="header_div"></div>
 			</header>
             <br>
 			<nav>
-				<a href="index.php" ><img  src="../icon/home1.png" >Home</a>
-				<a href=".html"><img  src="../icon/str.png" >Student Resources</a>
-				<a href="faculty.html"><img  src="../icon/Programinfo.png" >Faculty</a>
-				<a href="history.html"><img  src="../icon/hist.png" >History</a>
-                
-                <a class="searchnav"><img  src="../icon/search%201.png" ></a>
+				<div id="nav_div"></div>
 			</nav>
 			<main>
                 <div id=></div>
@@ -49,8 +44,8 @@
 				</div>
 			</main>
 			<footer>
-				//saved for Amir
+				<div id="footer_div"></div>
 			</footer>
-		</span>
+		</div>
 	</body>
 </html>
