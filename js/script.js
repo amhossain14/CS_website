07c8b47a33aced27b447009a9ef40f4099fa01a8
mattj122 <mattj122@gmail.com>
2016-11-10 20:39:48 -0600

    More fixes/changes

--- js/script.js
+++ js/script.js
@@ -2,31 +2,10 @@ window.onload = init;
 function init(){
 	var add = document.getElementById("add");
 	var change = document.getElementById("change");
-	add.onclick = makeRequest(../pages/add_update.php);
-	change.onclick = makeRequest(../pages/change_update.php);
-	makeRequest(../pages/header.html);
-	makeRequest(../pages/nav.html);
-	makeRequest(../pages/footer.html);
-	var httpRequest;
+	loadContent();
 }
-(function makeRequest(url){
-	httpRequest = new XMLHttpRequest();
-	if(!httpRequest){
-		alert('Giving up :( Cannot create an XMLHTTP instance');
-		return false;
-	}
-	httpRequest.onreadystatechange = alertContents;
-	httpRequest.open('GET', url);
-	httpRequest.send();
-	}
-	function alertContents(){
-		if(httpRequest.readyState ===XMLHttpRequest.DONE){
-			if(httpRequest.status === 200){
-				alert(httpRequest.responseText);
-			}
-			else{
-				alert('There was a problem with the request');
-			}
-		}
-	}
-})();
+function loadContent(){
+	$("#header_div").load("../pages/header.html");
+	$("#nav_div").load("../pages/nav.html");
+	$("#footer_div").load("../pages/footer.html");
+}
