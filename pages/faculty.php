4720455cb0b5a1502c29d49f9b5ac2fa984ff89b
mattj122 <mattj122@gmail.com>
2016-12-02 10:47:42 -0600

    More fixes

--- pages/faculty.php
+++ pages/faculty.php
@@ -7,7 +7,6 @@
 		<link rel="stylesheet" href="../css/style.css">
 		<link rel="icon" href="../back_logo/logow.png">
         <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Cuprum:400,400i,700,700i" rel="stylesheet">
-        
 	</head>
 	<body>
 		<div id="wrapper">
@@ -19,7 +18,6 @@
 				<div id="nav_div"></div>
 			</nav>
 			<main>
-                
                 <?php    
             $facdir = file("../faculty/faculty.txt"); //will put into array
             $faccount = count($facdir);   //  will count how many are there
@@ -94,10 +92,10 @@
 										<?php } ?>
 									</tr>
 								</table>
-                        <?php }
-						else{ ?>
-							<h4><?php print $nameprint." has not entered office hours for this semester" ?></h4>
-						<?php } ?>
+							<?php }
+							else{ ?>
+								<h4><?php print $nameprint." has not entered office hours for this semester" ?></h4>
+							<?php } ?>
 							</div>
 							<div class="image">
 								<img src="<?=$parent.$facdir[$i]?>/circle_tra.png" alt="pic" >
@@ -114,59 +112,58 @@
 						<div class="paragraph">	
 							<h3><?= $nameprint?></h3><br><p><?=$infoprint?></p>
 						<?php if(file_exists("$parent$facdirchop$hoursfile")){
-								$hoursprint= file("$parent$facdirchop$hoursfile"); ?>
-								<h4><?php print $nameprint." Office Hours" ?></h4>
-								<table>
-									<tr>
-										<th>Monday</th>
-										<th>Tuesday</th>
-										<th>Wednesday</th>
-										<th>Thursday</th>
-										<th>Friday</th>
-									</tr>
-									<tr rowspan="10">
-										<?php for($j = 0; $j < 10; $j+=2){ ?>
-												<td>
-												<?php 
-												for($k = 0; $k < count($hoursprint)-1; $k+=2){
-													$day = $hoursprint[$k];
-													switch($j){
-														case 0: 
-														if(chop($day) == "mon"){
-															print $hoursprint[$k+1]."<br/>";
-														}
-														break;
-														case 2: 
-														if(chop($day) == "tue"){
-															print $hoursprint[$k+1]."<br/>";
-														}
-														break;
-														case 4: 
-														if(chop($day) == "wed"){
-															print $hoursprint[$k+1]."<br/>";
-														}
-														break;
-														case 6: 
-														if(chop($day) == "thu"){
-															print $hoursprint[$k+1]."<br/>";
-														}
-														break;
-														case 8: 
-														if(chop($day) == "fri"){
-															print $hoursprint[$k+1]."<br/>";
-														}
-														break;
+							$hoursprint= file("$parent$facdirchop$hoursfile"); ?>
+							<h4><?php print $nameprint." Office Hours" ?></h4>
+							<table>
+								<tr>
+									<th>Monday</th>
+									<th>Tuesday</th>
+									<th>Wednesday</th>
+									<th>Thursday</th>
+									<th>Friday</th>
+								</tr>
+								<tr rowspan="10">
+									<?php for($j = 0; $j < 10; $j+=2){ ?>
+											<td>
+											<?php 
+											for($k = 0; $k < count($hoursprint)-1; $k+=2){
+												$day = $hoursprint[$k];
+												switch($j){
+													case 0: 
+													if(chop($day) == "mon"){
+														print $hoursprint[$k+1]."<br/>";
 													}
-												} ?>
-												</td>
-										<?php } ?>
-									</tr>
-								</table>
+													break;
+													case 2: 
+													if(chop($day) == "tue"){
+														print $hoursprint[$k+1]."<br/>";
+													}
+													break;
+													case 4: 
+													if(chop($day) == "wed"){
+														print $hoursprint[$k+1]."<br/>";
+													}
+													break;
+													case 6: 
+													if(chop($day) == "thu"){
+														print $hoursprint[$k+1]."<br/>";
+													}
+													break;
+													case 8: 
+													if(chop($day) == "fri"){
+														print $hoursprint[$k+1]."<br/>";
+													}
+													break;
+												}
+											} ?>
+											</td>
+									<?php } ?>
+								</tr>
+							</table>
 						</div>
 					<br></div>
-                <?php 
-                        $toggle-- ;
-						}
+                <?php $toggle-- ;
+					}
                 } 
 			} ?>
 			</main>
