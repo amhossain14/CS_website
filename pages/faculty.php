e4435dbfc434515280cfef50ba337247d248e015
mattj122 <mattj122@gmail.com>
2016-12-02 10:44:16 -0600

    Fixed Faculty page

--- pages/faculty.php
+++ pages/faculty.php
@@ -41,7 +41,6 @@
                     
                     if( $toggle===1){
                         ?>
-<<<<<<< HEAD
 						<div class="sections">
 							<div class="paragraph">	
 								<h3><?= $nameprint?></h3><br><p><?=$infoprint?></p>
@@ -104,29 +103,7 @@
 								<img src="<?=$parent.$facdir[$i]?>/circle_tra.png" alt="pic" >
 							</div>
 						<br></div>
-=======
-            <div class="block">
-                <div class="sections">
-                    <div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                    </div>
-					<div class="image">
-						<img src="<?=$parent.$facdir[$i]?>/circle_tra.png" alt="pic" >
-					</div>
-                </div>
-                    <div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                    </div>
-           
-                <div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                </div>
-			<br></div>
->>>>>>> origin/master
-                        
                         <?php
-                        
-<<<<<<< HEAD
                         $toggle++ ;
                    } 
 				   else{ ?>
@@ -188,30 +165,8 @@
 						</div>
 					<br></div>
                 <?php 
-=======
-                    <div class="block">
-
-                <div class="sections">
-                    <div class="image">
-						<img src="<?=$parent.$facdir[$i]?>/circle_tra.png" alt="pic" >
-					</div>
-					<div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                    </div>	
-                </div>
-                <div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                </div>
-            </div>
-                <div class="paragraph">	
-						<p> <?= $nameprint?> <br> <?=$infoprint?></p>
-                </div>
-            </div>
-			<br></div>
-    
-                <?php
+                    
                         print"<br>";
->>>>>>> origin/master
                         $toggle-- ;
 						}
                 } 
